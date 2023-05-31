48f6348f1d6c3e1659af291da6d2d1b44c35e4d8
Murat Can Taner <muratcantaner@hotmail.com>
2023-06-01 02:38:32 +0300

    ft_range v2

--- deneme3/ft_range.c
+++ deneme3/ft_range.c
@@ -1,22 +1,51 @@
 #include <stdlib.h>
 
-
+/*
 int	*ft_range(int start, int end)
 {
-	int rangesize = (end-start) +1;
+	int ascend = 1;
+	int i = 0;
+
+	if (end<start)
+	{
+		ascend = -1;
+	}
+
+	int rangesize = ((end - start) * ascend) +1;
 	int *range = malloc(4*rangesize);
+	while(i<rangesize)
+	{
+		range[i] = start+(i * ascend);
+		i++;
+	}
+
+	return range;
+}
+*/
+int *ft_range(int start, int end)
+{
+	int increase = 1;
+
+	if(end<start)
+		increase = -1;
+
+	int rangesize = increase * (end-start) +1;
+	
+	int *range = malloc(4 * rangesize);
 	
 	int i = 0;
 	while(i<rangesize)
 	{
-		range[i] = start+i;
+		range[i] = start+(i * increase);
 		i++;
 	}
 
 	return range;
 }
 
-/*#include <stdio.h>
+
+// /*
+#include <stdio.h>
 int main(int argc, char **argv)
 {
 	int start = atoi(argv[1]);
@@ -30,4 +59,4 @@ int main(int argc, char **argv)
 	while(i<rangesize)
 		printf("%d,",range[i++]);
 }
-*/
+// */
