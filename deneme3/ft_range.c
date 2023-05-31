1d12081e166e2df692868f1ffaee3eb44bf26c4c
Murat Can Taner <muratcantaner@hotmail.com>
2023-06-01 02:08:51 +0300

    ft_range

---
+++ deneme3/ft_range.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+
+int	*ft_range(int start, int end)
+{
+	int rangesize = (end-start) +1;
+	int *range = malloc(4*rangesize);
+	
+	int i = 0;
+	while(i<rangesize)
+	{
+		range[i] = start+i;
+		i++;
+	}
+
+	return range;
+}
+
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	int start = atoi(argv[1]);
+	int end = atoi(argv[2]);
+
+	int rangesize = (end - start) +1;
+	int *range = ft_range(start, end);
+
+	int i = 0;
+
+	while(i<rangesize)
+		printf("%d,",range[i++]);
+}
