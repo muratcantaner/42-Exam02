4c7a5c2de2a9b243c4fc1033c683f109ae80cb02
Murat Can Taner <muratcantaner@hotmail.com>
2023-06-01 10:28:07 +0300

    ft_strcspn v3.

---
+++ deneme4/ft_rrange.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+
+int	*ft_rrange(int start, int end)
+{
+	int increase = 1;
+
+	int tmp = start;
+	start = end;
+	end = tmp;
+
+	if(end<start)
+		increase = -1;
+	
+	int rangesize = (end-start) * increase +1;
+	int *rrange = malloc(rangesize * 4);
+
+	int i = 0;
+	while(i<rangesize)
+	{
+		rrange[i] = start + (i*increase);
+		i++;
+	}
+	return rrange;
+}
+/*
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int increase = 1;
+	int start = atoi(argv[1]);
+	int end = atoi(argv[2]);
+	if(end<start)
+		increase = -1;
+	
+	int rangesize = (end-start) * increase +1;
+
+	int *rrange = ft_rrange(start,end);
+
+	int i = 0;
+	while(i<rangesize)
+		printf("%d,",rrange[i++]);
+}
+*/
